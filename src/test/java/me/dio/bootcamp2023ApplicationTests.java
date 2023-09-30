57e2cf4befbf11f6d2591b5931429925e1414e64
JuniorLima36 <juninho_livre2011@hotmail.com>
2023-09-30 03:40:16 -0300

    deploy

---
+++ src/test/java/me/dio/bootcamp2023ApplicationTests.java
@@ -0,0 +1,11 @@
+package me.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class bootcamp2023ApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
